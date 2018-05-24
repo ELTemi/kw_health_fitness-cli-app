d23ae6c252eb54497875b715572ca0b7a418b5b2
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-24 20:10:43 +0000

    added the scraper.rb file. designed the welcome and list methods in the cli

--- lib/health_fitness_deals/cli.rb
+++ lib/health_fitness_deals/cli.rb
@@ -1,5 +1,24 @@
 class HealthFitnessDeals::CLI
   def call
-    puts "Welcome to the Deals app for Health and Fitness in the KW area"
+    start
   end
+
+  def start
+    puts "Welcome to the Deals app for Health and Fitness in the KW area"
+    puts "Here are available deals currently trending"
+    list
+  end
+
+  def list
+    puts <<-DOC.gsub /^\s*/, ""
+      1. Vincent Optical - 90% off Eyewear
+      2. Stronger Forever - 73% off Group Training Classes
+      3. Free 60-Days from Daily Burn - Daily Burn
+    DOC
+  end
+
+  def method_name
+
+  end
+
 end
