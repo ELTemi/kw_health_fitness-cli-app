94b5995e197a943314f6b6980188f10ed9b817af
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-25 18:59:36 +0000

    defined the scrape index class method

--- lib/health_fitness_deals/scraper.rb
+++ lib/health_fitness_deals/scraper.rb
@@ -1,10 +1,26 @@
-class HumanFitnessDeals::DealsScrapper
+class HealthFitnessDeals::DealsScrapper
 
   def self.scrape_web_index
     doc = Nokogiri::HTML(open("https://www.livingsocial.com/browse/kitchener-waterloo?category=health-and-fitness"))
+    list_doc = doc.search("div.cui-udc-title-with-subtitle").text
     binding.pry
-    deals
+    all_deals = []
+    list_doc.each do |deal|
+      all_deals.push({
+        :title => deal.search("div.cui-udc-title-with-subtitle").text.strip,
+        :subtitle => deal.search("div.cui-udc-subtitle").text.strip,
+        :url => deal.search("a").attr("href").value
+        })
+
   end
 
+  def self.scrape_details
+    scrape_web_index.each do |deal|
 
+    end
+  end
 end
+
+:title => deal.search("div.cui-udc-title-with-subtitle").text,
+:subtitle => deal.search("div.cui-udc-subtitle").text,").text,
+:url => deal.search("a").attr("href").value
