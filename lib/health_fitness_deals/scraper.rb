45812a81ec2a762c06d467a2f525f33e2119339a
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-25 19:49:47 +0000

    defined the scraped_details method

--- lib/health_fitness_deals/scraper.rb
+++ lib/health_fitness_deals/scraper.rb
@@ -1,4 +1,6 @@
 class HealthFitnessDeals::DealsScrapper
+  attr_accessor :url
+
 
   def self.scrape_web_index
     doc = Nokogiri::HTML(open("https://www.livingsocial.com/browse/kitchener-waterloo?category=health-and-fitness"))
@@ -11,16 +13,18 @@ class HealthFitnessDeals::DealsScrapper
         :subtitle => deal.search("div.cui-udc-subtitle").text.strip,
         :url => deal.search("a").attr("href").value
         })
-
+      end
+      all_deals
   end
 
-  def self.scrape_details
-    scrape_web_index.each do |deal|
-
-    end
+  def self.scrape_details(url)
+    doc = Nokogiri::HTML(url)
+    details = doc.search("div.module-body")
+    scraped_deal = {}
+    details.each do |deal|
+      scraped_deal[:title] = deal.search("h1#deal-title").text.strip
+      scraped_deal[:orginal_price] = deal.search("div.value-source-wrapper div.breakout-option-value").text.strip
+      scraped_deal[:discount_price] = deal.search("div.price-discount-wrapper div.breakout-option-price").text.strip
+      scraped_deal[:description] = deal.search("section p").text.strip
   end
 end
-
-:title => deal.search("div.cui-udc-title-with-subtitle").text,
-:subtitle => deal.search("div.cui-udc-subtitle").text,").text,
-:url => deal.search("a").attr("href").value
